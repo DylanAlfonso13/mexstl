fcc89f6f686028479263e24861457d8e18ee3e85
Dylan Alfonso <alfonso.d@wustl.edu>
2025-02-28 17:53:16 -0600

    Removed pili

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -39,11 +39,11 @@ export default function About() {
             title="PhD Candidate in Hispanic Studies"
             imageURL="/sarah_hernandez_picture.png"
           />
-          <AboutCard
+          {/* <AboutCard
             name="María de Pilar Cuairán Chavarría"
             title="PhD Candidate in Hispanic Studies"
             imageURL="/maria_de_pilar_picture.png"
-          />
+          /> */}
           <AboutCard
             name="Elaine Peña"
             title="Professor of Performing Arts, American Culture Studies, and Anthropology"
