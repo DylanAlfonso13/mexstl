42126eb0184ce965cfbe61b843a0a9c4f15451ba
Dylan Alfonso <alfonso.d@wustl.edu>
2025-03-28 18:55:39 -0500

    Media finished

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,8 +1,11 @@
+"use client";
 import Nav from "@/components/Nav";
 import TitleText from "@/components/TitleText";
 import SpotlightCard from "@/components/SpotlightCard";
+import { useLanguage } from "@/context/LanguageContext";
 
 export default function Media() {
+  const { language } = useLanguage();
   return (
     <div>
       <Nav />
@@ -10,8 +13,16 @@ export default function Media() {
         <TitleText text="Media" />
         <div className="mt-10"></div>
         <SpotlightCard
-          title="Public Humanities Lab Takes on Reparative Memory"
-          description="Explore the article and podcast that mentions our work with the Public Humanities Lab on reparative memory in Missouri."
+          title={
+            language === "en"
+              ? "Public Humanities Lab Takes on Reparative Memory"
+              : "Laboratorio de Humanidades Públicas se Enfoca en la Memoria Reparativa"
+          }
+          description={
+            language === "en"
+              ? "Explore the article and podcast that mentions our work with the memory for the future lab at WashU."
+              : "Explora el artículo y el podcast que menciona nuestro trabajo con el laboratorio de memoria para el futuro en WashU."
+          }
           imageSrc="/reparative-memory.png" // Change this to a real image URL
           articleUrl="https://www.kbia.org/missouri-news/2023-04-27/public-humanities-lab-takes-on-reparative-memory-to-uncover-the-legacy-of-slavery-in-missouri"
         />
